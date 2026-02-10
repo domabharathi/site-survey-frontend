f5b0bdc5118002ad2e22a6d57dc0713ea6d45b6e
domabharathi <domabharathi074@gmail.com>
2026-02-10 23:23:14 +0530

    Update spaceService.js

--- src/services/spaceService.js
+++ src/services/spaceService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { authHeader } from "./authHeader";
 
-const API_URL = "https://site-survey-tool-production.up.railway.app/api/spaces";
+const API_URL = "https://site-survey-backend-production.up.railway.app/api/spaces";
 
 export const createSpace = (floorId, space) => {
   return axios.post(
