3d61a9c84e05997803808970f2ce55869bba01ca
domabharathi <domabharathi074@gmail.com>
2026-02-10 23:22:14 +0530

    Update dashboardService.js

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { authHeader } from "./authHeader";
 
-const API_URL = "https://site-survey-tool-production.up.railway.app/api/reports";
+const API_URL = "https://site-survey-backend-production.up.railway.app/api/reports";
 
 export const getDashboardSummary = () => {
   return axios.get(
